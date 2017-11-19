afddbea3fe31c56604480c11c1f8e840f5f84c88
jocaro97 <johanna_capote_robayna@hotmail.com>
2017-11-19 13:27:27 +0100

    aniadido Cliente, no funciona el servidor da error el globar

--- src/Cipher/Cliente.py
+++ src/Cipher/Cliente.py
@@ -1,8 +1,30 @@
 import socket
 import sys
 
-# Creación del socket
-sock = socket.socket(socket.family=AF_INET, socket.SOCK_STREAM)
+# Creacion del socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = (localhost, 8989)
-print('Conectándose a %s puerto %s') % server_address
+server_address = ('localhost', 8989)
+print('Conectandose a %s puerto %s') % server_address
+
+sock.connect(server_address)
+
+
+try:
+    # Lectura de las opciones
+
+    data = sock.recv(1024)
+    print('received', repr(data))
+
+    # Send data
+    numero = input()
+    print >>sys.stderr, 'sending "%s"' % numero
+    sock.sendall(str(numero))
+
+    # Received
+    data = sock.recv(1024)
+    print('received', repr(data))
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close
